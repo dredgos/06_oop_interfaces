c733e48b2c72d0c42a9f9c711e4f8f89eed60fac
Paul Dredge <dredgos@googlemail.com>
2021-02-12 13:01:00 +0000

    Challenge 1 completed

---
+++ app/Shapes/Circle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Shapes;
+
+
+class Circle implements ShapeInterface
+{
+    private $radius;
+
+    public function __construct($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function area()
+    {
+        return pi() * ($this->radius * $this->radius);
+    }
+}
